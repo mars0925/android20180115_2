85e559c1bf9f41a3d53f83c35593acfb259df453
MarsChang <mars09250148@yahoo.com.tw>
2018-01-15 15:06:21 +0800

    AsyncTask 進行圖片下載

--- app/src/main/java/com/mars/android20180115_2/MainActivity.java
+++ app/src/main/java/com/mars/android20180115_2/MainActivity.java
@@ -18,6 +18,7 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ImageView img;
@@ -103,23 +104,21 @@ public class MainActivity extends AppCompatActivity {
         MyTask task = new MyTask();
         task.execute(10);
     }
-    //建立AsyncTask
-    class MyTask extends AsyncTask<Integer, Integer, String>
-    {
+    //建立AsyncTask class 來用
+    class MyTask extends AsyncTask<Integer, Integer, String> {
         @Override
         //Integer... 參數不確定
         //這個是在背後執行 副執行緒
         protected String doInBackground(Integer... integers) {
             int i;
 
-            for(i = 0 ;i<=integers[0];i++)
-            {
+            for (i = 0; i <= integers[0]; i++) {
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                Log.d("task","doinblackground, I:" + i);
+                Log.d("task", "doinblackground, I:" + i);
                 publishProgress(i);//公佈進度
             }
             return "OKay";
@@ -146,6 +145,74 @@ public class MainActivity extends AppCompatActivity {
             super.onProgressUpdate(values);
             tv2.setText(String.valueOf(values[0]));
         }
+        }
+
+    public void click3(View v)
+    {
+        MyImageTask task = new MyImageTask();
+        task.execute("https://5.imimg.com/data5/UH/ND/MY-4431270/red-rose-flower-500x500.jpg");
+    }
+         /*
+        AsyncTask<Params, Progress, Result>，這是基本的架構，使用泛型來定義參數，
+        泛型意思是，你可以定義任意的資料型態給他。
+        Params ： 參數，你要餵什麼樣的參數給它。
+        Progress ：進度數值
+        Result ： 結果，你希望這個背景任務最後會有什麼樣的結果回傳給你。
+         */
+        //String 因為要傳網址給他
+        //Integer 進度數值就是用整數就可以
+        //我們要下載的是圖片 所以用Bitmap
+         class MyImageTask extends AsyncTask<String, Integer, Bitmap>
+         {
+             @Override
+             protected void onPostExecute(Bitmap bitmap) {
+                 super.onPostExecute(bitmap);
+                 img.setImageBitmap(bitmap);
+             }
+
+             @Override
+             protected void onProgressUpdate(Integer... values) {
+                 super.onProgressUpdate(values);
+             }
+
+             @Override
+             protected Bitmap doInBackground(String... strings) {
+                 String str_url = strings[0];
+                 URL url;
+                 try {
+                     url = new URL(str_url);
+                     HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                     conn.setRequestMethod("GET");
+                     conn.connect();
+                     InputStream inputStream = conn.getInputStream();
+                     ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                     byte[] buf = new byte[1024];
+                     final int totalLength = conn.getContentLength();
+                     int sum = 0;
+                     int length;
+                     while ((length = inputStream.read(buf)) != -1)
+                     {
+                         sum += length;
+                         final int tmp = sum;
+                         bos.write(buf, 0, length);
+                     }
+                     byte[] results = bos.toByteArray();
+                     final Bitmap bmp = BitmapFactory.decodeByteArray(results, 0, results.length);
+                     return bmp;
+                 } catch (MalformedURLException e) {
+                     e.printStackTrace();
+                 } catch (ProtocolException e) {
+                     e.printStackTrace();
+                 } catch (IOException e) {
+                     e.printStackTrace();
+                 }
+                 return null;
+             }
+         }
+
+
     }
 
-}
+
+
+
